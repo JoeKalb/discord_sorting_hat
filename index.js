a003a8261cf8999f863d13f4ce75714b69041109
Joseph Kalb <kalb.joseph@gmail.com>
2020-01-03 19:09:09 -0800

    added remove feature for just me

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ client.once('ready', () => {
         console.log(`${guild.name} roles:`)
         console.log(guild.roles.forEach(role => {
             console.log(`\t${role.name}\t${role.id}`)
+
+            if(role.id === '264164239797649408'){
+                role.members.forEach(member => {
+                    console.log(`${member.displayName}: ${member.id}`)
+                })
+            }
         }))
         console.log(`${guild.name} channels:`)
         console.log(guild.channels.forEach(channel => {
@@ -32,7 +38,6 @@ client.login(TOKEN);
 
 
 client.on('message', message => {
-    console.log(message.channel.id)
 	if (message.content === '!ping') {
         // send back "Pong." to the channel the message was sent in
         //message.channel.send('Pong.');
@@ -43,29 +48,36 @@ client.on('messageReactionAdd', reaction => {
     const {message, users, emoji} = reaction
     if(message.channel.id === '662128396599558175' // change to #sorting_hat
     && message.guild.id === '160072797475962881' // change to thaButtCrew
-    && isMod(message.guild, users)
-    && !hasHouse(message.member))
+    && isMod(message.guild, users)){
         if(!hasHouse(message.member)){
             if(HP_EMOTE_ID_TO_ROLES.hasOwnProperty(emoji.id)){
                 console.log(`Would add to role ${HP_EMOTE_ID_TO_ROLES[emoji.id].name}`)
                 // add roles here
-                /*
+                
                 message.member.addRole(HP_EMOTE_ID_TO_ROLES[emoji.id].role_id)
                 .then(res => {
                     console.log(`${message.member.displayName} has been added to ${HP_EMOTE_ID_TO_ROLES[emoji.id].name}.`)
-                    //message.reply(`${HP_EMOTE_ID_TO_ROLES[emoji.id].name.toUpperCase()}!!!`)
+                    message.reply(`${HP_EMOTE_ID_TO_ROLES[emoji.id].name.toUpperCase()}!!!`)
                 }).catch(err => {
                     console.log(`Error Giving Role: ${err}`)
                 })
-                */
-               
             }
         }else{
-            console.log(`Emote ID: ${emoji.id}`)
-            console.log(`Emote Name: ${emoji.name}`)
-            console.log('will remove the house role from the person')
+            if('356889743239413763' === emoji.id && isJoeFish(users)){
+                console.log(`Emote ID: ${emoji.id}`)
+                console.log(`Emote Name: ${emoji.name}`)
+                console.log('will remove the house role from the person')
+                let current_house_id;
+                message.member.roles.forEach(role => {
+                    if(HP_ROLE_IDS.includes(role.id)){
+                        current_house_id = role.id
+                    }
+                })
+
+                message.member.removeRole(current_house_id)
+            }
         }
-        
+    }
 })
 
 const HP_EMOTE_ID_TO_ROLES = { // change to HP emotes and roles
@@ -119,6 +131,17 @@ let hasHouse = (member) => {
             result = true 
             console.log(`${member.displayName} already has a role`)
         }
+    })
+
+    return result
+}
+
+let isJoeFish = (users) => {
+    let result = false
+
+    users.forEach(user => {
+        if(user.id === '91370189366530048')
+            result = true
     })
 
     return result
